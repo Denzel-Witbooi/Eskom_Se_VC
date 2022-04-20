428690cf450738dbabef35871649a163d48c23a2
Denzel Witbooi <dwitbooi28@gmail.com>
2022-04-20 08:09:43 +0200

    inital

--- app/src/main/java/com/varsitycollege/eskomsevc/MainActivity.java
+++ app/src/main/java/com/varsitycollege/eskomsevc/MainActivity.java
@@ -1,14 +1,47 @@
 package com.varsitycollege.eskomsevc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", "Tokyo");
+        data.put("country", "Japan");
+        data.put("timestamp", FieldValue.serverTimestamp());
+
+
+        db.collection("cities")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
